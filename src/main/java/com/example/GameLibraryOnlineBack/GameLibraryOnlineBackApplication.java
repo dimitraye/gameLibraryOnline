4186e2a1697ddf107786bad176952276c9959b05
Dimitri MOMPEROUSSE <momperousse.dimitri.pro@gmail.com>
2024-12-06 10:13:02 +0100

    First commit

---
+++ src/main/java/com/example/GameLibraryOnlineBack/GameLibraryOnlineBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.GameLibraryOnlineBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLibraryOnlineBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLibraryOnlineBackApplication.class, args);
+	}
+
+}
